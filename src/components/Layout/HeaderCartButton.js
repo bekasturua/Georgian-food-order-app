7771cfbeeb300e606740cb52a9ce24cf0eb6c2f4
Beka Sturua <beqasturua3@gmail.com>
2022-06-23 12:51:50 +0400

    add components

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import CartIcon from "../Cart/CartIcon";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button>
+      <span>
+        <CartIcon />
+      </span>
+      <span></span>
+      <span></span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
